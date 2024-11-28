f76a8a39f95593e989cc9e70c504a82d1f89ce40
Pedro Affonso <pedroaffonsosilvamarques@gmail.com>
2024-11-28 09:03:20 -0300

    removing old files after reorganization

--- src/components/Logo/index.tsx
+++
@@ -1,7 +0,0 @@
-import { TextLogo } from './styles';
-
-export default function Logo() {
-    return (
-        <TextLogo>VagaCerta</TextLogo>
-    );
-}
--- src/components/Logo/styles.ts
+++
@@ -1,8 +0,0 @@
-import { styled } from 'styled-components/native';
-
-export const TextLogo = styled.Text`
-  font-size: ${({theme})=>theme.FONT_SIZE.XL}px;
-  font-weight: 700;
-  text-align: center;
-  color: ${({theme})=>theme.COLORS.GREEN};
-`;
